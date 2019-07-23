8ef47d00fd984183cce3a21a3eaec81212f41c76
John Bowen <jbowen7@gmail.com>
2019-07-23 18:55:37 +0000

    - manager, query: added docstring and TODO (how to handle cache)

--- src/django_extmodels/manager.py
+++ src/django_extmodels/manager.py
@@ -8,4 +8,12 @@ class ExtManager(models.Manager):
 		return ExtQuerySet(self.model, using=self._db)
 
 	def cached_count(self, *args, **kwargs):
+		"""
+		Like self.count(), except, it first checks the cache.
+		If the value is not found, a SELECT COUNT() is performed
+		and that value is stored in the cache.
+
+		:param recount: if True then perform count and update cache with `SELECT COUNT()`
+		:returns: number of records as an integer
+		"""
 		return self.get_queryset().cached_count(*args, **kwargs)
--- src/django_extmodels/query.py
+++ src/django_extmodels/query.py
@@ -5,9 +5,18 @@ from django.db import models
 
 from django_extmodels.utils import generate_count_query, get_extmeta
 
+#TODO:jbowen7 the cache to use should be determined from settings
 
 class ExtQuerySet(models.QuerySet):
 	def cached_count(self, recount=False):
+		"""
+		Like self.count(), except, it first checks the cache.
+		If the value is not found, a SELECT COUNT() is performed
+		and that value is stored in the cache.
+
+		:param recount: if True then perform count and update cache with `SELECT COUNT()`
+		:returns: number of records as an integer
+		"""
 		# Key to use for cache
 		query = generate_count_query(self.query)
 		key = f"{query}; using={self.db}"
