3a106c72d664a69c20f4551b13a8ff7657cd1aae
John Bowen <jbowen7@gmail.com>
2019-07-23 21:59:48 +0000

    manager.ExtManager changed definition Formerly, it inherited from models.Manager This was a problem because its method `cached_count` relied on `get_queryset()` which returned a ExtQuerySet or a subclass thereof. The issue would be exposed when the ExtManager was subclassed and its `get_queryset` method overwritten with one that returned a non ExtQuerySet subclass. This then, broke the `cached_count()` method. The solution was to define the class in the same manner which models.Manager is defined. This assures the class to has access to the extra methods defined on ExtQuerySet, namely `cached_count`

--- src/django_extmodels/manager.py
+++ src/django_extmodels/manager.py
@@ -1,19 +1,10 @@
-from django.db import models
+from django.db.models.manager import BaseManager
 
 from django_extmodels.query import ExtQuerySet
 
 
-class ExtManager(models.Manager):
-	def get_queryset(self):
-		return ExtQuerySet(self.model, using=self._db)
-
-	def cached_count(self, *args, **kwargs):
-		"""
-		Like self.count(), except, it first checks the cache.
-		If the value is not found, a SELECT COUNT() is performed
-		and that value is stored in the cache.
-
-		:param recount: if True then perform count and update cache with `SELECT COUNT()`
-		:returns: number of records as an integer
-		"""
-		return self.get_queryset().cached_count(*args, **kwargs)
+class ExtManager(BaseManager.from_queryset(ExtQuerySet)):
+	"""
+	A subclassable Manager
+	"""
+	pass
